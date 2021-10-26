74bcedc80013f1942c61e828b6fa89001bda6e68
Dmytrij Demyanenko <dingofleep@gmail.com>
2021-10-26 13:56:13 +0300

    Updates

---
+++ precache-manifest.2e9605062adaebd10ac0bccd95cb726a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99131bee8a3713b9fc251aa93fc08180",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "bd0f38c7794a7fd70fd7",
+    "url": "/SocialNetwork/static/css/main.39b929c7.chunk.css"
+  },
+  {
+    "revision": "63f413e0f06566dd92c5",
+    "url": "/SocialNetwork/static/js/2.80ffa14e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SocialNetwork/static/js/2.80ffa14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0f38c7794a7fd70fd7",
+    "url": "/SocialNetwork/static/js/main.0d17a4f9.chunk.js"
+  },
+  {
+    "revision": "e25694bbfba73a0d4bc8",
+    "url": "/SocialNetwork/static/js/runtime-main.090bd75b.js"
+  },
+  {
+    "revision": "a33969313e27c398f2230c7675e3ee5c",
+    "url": "/SocialNetwork/static/media/arrowDown.a3396931.svg"
+  },
+  {
+    "revision": "47b57fccdb320b526872b54dd8126523",
+    "url": "/SocialNetwork/static/media/likedHeart.47b57fcc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SocialNetwork/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6108e34c71ac2b103a18bc392bd1429",
+    "url": "/SocialNetwork/static/media/unlikedHeart.f6108e34.svg"
+  }
+]);
